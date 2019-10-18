97c458ccd41cc0ee6cf6b4e3a0969374b255c0e6
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-10-18 14:22:12 -0700

    Update automobile.js

--- automobile.js
+++ automobile.js
@@ -1,17 +1,18 @@
-function Automobile( year, make, model, type ){
+function Automobile( year, make, model, type, expression ){
     this.year = year; //integer (ex. 2001, 1995)
     this.make = make; //string (ex. Honda, Ford)
     this.model = model; //string (ex. Accord, Focus)
     this.type = type; //string (ex. Pickup, SUV)
+    this.expression = expression;
 }
 
 var automobiles = [ 
-    new Automobile(1995, "Honda", "Accord", "Sedan"),
-    new Automobile(1990, "Ford", "F-150", "Pickup"),
-    new Automobile(2000, "GMC", "Tahoe", "SUV"),
-    new Automobile(2010, "Toyota", "Tacoma", "Pickup"),
-    new Automobile(2005, "Lotus", "Elise", "Roadster"),
-    new Automobile(2008, "Subaru", "Outback", "Wagon")
+    new Automobile(1995, "Honda", "Accord", "Sedan", true),
+    new Automobile(1990, "Ford", "F-150", "Pickup", true),
+    new Automobile(2000, "GMC", "Tahoe", "SUV", true),
+    new Automobile(2010, "Toyota", "Tacoma", "Pickup", true),
+    new Automobile(2005, "Lotus", "Elise", "Roadster", true),
+    new Automobile(2008, "Subaru", "Outback", "Wagon", true)
     ];
 
 /*This function sorts arrays using an arbitrary comparator. You pass it a comparator and an array of objects appropriate for that comparator and it will return a new array which is sorted with the largest object in index 0 and the smallest in the last index*/
@@ -86,11 +87,11 @@ function typeComparator( auto1, auto2){
 }
 
 /*Your program should output the following to the console.log, including the opening and closing 5 stars. All values in parenthesis should be replaced with appropriate values. Each line is a seperate call to console.log.*/
-function logMe (expression){
-    if (expression){
-        console.log( year make model type);
+function logMe (){
+    if (automobile.expression){
+        console.log(automobile.year+' '+automobile.make+' '+automobile.model+' '+automobile.type);
     } else {
-        console.log(year make model);
+        console.log(automobile.year+' '+automobile.make+' '+automobile.model);
     }
 }
 /*Each line representing a car should be produced via a logMe function. This function should be added to the Automobile class and accept a single boolean argument. If the argument is 'true' then it prints "year make model type" with the year, make, model and type being the values appropriate for the automobile. If the argument is 'false' then the type is ommited and just the "year make model" is logged.
@@ -117,9 +118,9 @@ As an example of the content in the parenthesis:
 
 console.log('*****');
 console.log('The cars sorted by year are:');
-console.log(sortArr(yearComparator, automobiles));
+logMe(sortArr(yearComparator, automobiles));
 console.log('The cars sorted by make are:');
-console.log(sortArr(makeComparator, automobiles));
+logMe(sortArr(makeComparator, automobiles));
 console.log('The cars sorted by type are:');
-console.log(sortArr(typeComparator, automobiles));
+logMe(sortArr(typeComparator, automobiles));
 console.log('*****');
