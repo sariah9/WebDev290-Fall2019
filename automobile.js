d168c4fd9687e7aaec015ead23a835ace7b673d8
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-10-18 13:52:31 -0700

    Update automobile.js

--- automobile.js
+++ automobile.js
@@ -17,16 +17,23 @@ var automobiles = [
 /*This function sorts arrays using an arbitrary comparator. You pass it a comparator and an array of objects appropriate for that comparator and it will return a new array which is sorted with the largest object in index 0 and the smallest in the last index*/
 function sortArr( comparator, array ){
     /*your code here*/
-    for (let i = 0; i < array.length; i++) {
-        let result = [];
-        if (comparator(array[i], array[i + 1])){
-            let hold = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = hold;
+    let result = [];
+    let sort = function(array){
+        boolean sorted = false;
+        let temp;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (comparator(array[i], array[i + 1]) == 1) {
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    sorted = false;
+                }
+            }
         }
-        result.push(sort);
     }
-    return result;
+    //how to use comparator?
 }
 
 /*A comparator takes two arguments and uses some algorithm to compare them. If the first argument is larger or greater than the 2nd it returns true, otherwise it returns false. Here is an example that works on integers*/
@@ -43,19 +50,20 @@ function exComparator( int1, int2){
 /*This compares two automobiles based on their year. Newer cars are "greater" than older cars.*/
 function yearComparator(auto1, auto2){
     /* your code here*/
-    automobiles.sortArr(function(auto1, auto2){return auto1.year - auto2.year});
+   if (auto1.year >= auto2.year){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /*This compares two automobiles based on their make. It should be case insensitive and makes which are alphabetically earlier in the alphabet are "greater" than ones that come later (from A-Z).*/
 function makeComparator(auto1, auto2){
     /* your code here*/
-    automobiles.sortArr(function(auto1, auto2){
-        let x = auto1.make.toLowerCase();
-        let y = auto2.make.toLowerCase();
-        if (x < y) {return -1;}
-        if (x > y) {return 1;}
-        return 0;
-    });        
+    let x = auto1.make.toLowerCase();
+    let y = auto2.make.toLowerCase();
+    if (x >= y) {return true;}   
+    else { return false;}       
 }
 
 /*This compares two automobiles based on their type. The ordering from "greatest" to "least" is as follows: roadster, pickup, suv, wagon, (types not otherwise listed). It should be case insensitive. If two cars are of equal type then the newest one by model year should be considered "greater".*/
@@ -69,18 +77,23 @@ function typeComparator( auto1, auto2){
         else {n = 0;}
         return n;
     }
-    automobiles.sortArr(function(auto1, auto2){
-        let x = order(auto1.type.toLowerCase());
-        let y = order(auto2.type.toLowerCase());   
-        if (x < y) {return -1;}
-        else if (x > y) {return 1;}
-        else {yearComparator(x, y);return 0;}
-    });
+    let x = order(auto1.type.toLowerCase());
+    let y = order(auto2.type.toLowerCase());   
+    if (x > y) {return true;}
+    else if (x < y) {return false;}
+    else {yearComparator(auto1, auto2);}
 }
 
-/*Your program should output the following to the console.log, including the opening and closing 5 stars. All values in parenthesis should be replaced with appropriate values. Each line is a seperate call to console.log.
-
-Each line representing a car should be produced via a logMe function. This function should be added to the Automobile class and accept a single boolean argument. If the argument is 'true' then it prints "year make model type" with the year, make, model and type being the values appropriate for the automobile. If the argument is 'false' then the type is ommited and just the "year make model" is logged.
+/*Your program should output the following to the console.log, including the opening and closing 5 stars. All values in parenthesis should be replaced with appropriate values. Each line is a seperate call to console.log.*/
+console.log('*****');
+console.log('The cars sorted by year are:');
+console.log();
+console.log('The cars sorted by make are:');
+console.log();
+console.log('The cars sorted by type are:');
+console.log();
+console.log('*****');
+/*Each line representing a car should be produced via a logMe function. This function should be added to the Automobile class and accept a single boolean argument. If the argument is 'true' then it prints "year make model type" with the year, make, model and type being the values appropriate for the automobile. If the argument is 'false' then the type is ommited and just the "year make model" is logged.
 
 *****
 The cars sorted by year are:
