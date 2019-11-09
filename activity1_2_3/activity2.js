33b63f4a796e94e7c4194572fcf65c90af0e7b4c
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-11-08 22:22:53 -0800

    Create activity2.js

---
+++ activity1_2_3/activity2.js
@@ -0,0 +1,10 @@
+//Activity 2
+console.log("Lets make sure JavaScript is working.");
+var name = "Sariah"; //Replace this with your first name
+console.log("The unicode characters of your name are:")
+for (var i = 0; i < name.length; i++){
+	console.log(name.charCodeAt(i));
+}
+console.log("Copy and paste these values for activity credit.")
+
+//run this in the console with node.js
