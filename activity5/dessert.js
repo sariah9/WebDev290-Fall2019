8e5b3e989e90662785c4efedf049f23372c81bdf
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-11-08 22:32:42 -0800

    Update dessert.js

--- activity5/dessert.js
+++ activity5/dessert.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', bindButtons);
 
+function bindButtons(){
+  document.getElementById('urlSubmit').addEventListener('click', function(event){
+    var req = new XMLHttpRequest();
+    var payload = {dessert:null};
+    payload.dessert = document.getElementById('dessert').value;
+    req.open('POST', 'http://httpbin.org/post', true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.send(JSON.stringify(payload));
+    var response = JSON.parse(req.responseText);
+    document.getElementById('favDessert').textContent = response.dessert;
+    event.preventDefault();
+  });
+}
