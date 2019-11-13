123cf662eacfdcf10ec1ea330f198a2986c4762d
Sariah Bunnell <33461825+sariah9@users.noreply.github.com>
2019-11-13 15:56:12 -0800

    Add files via upload

---
+++ assignment5/app.js
@@ -0,0 +1,62 @@
+/*Sources Cited: Instructor. Boilerplate Code. Week 7 Lectures. 
+http://eecs.oregonstate.edu/ecampus-video/CS290/core-content/hello-node/hello-node.html*/
+
+var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 4312);
+
+app.get('/',function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  res.render('getForm', context);
+  res.type('text/plain');
+  res.render('main.handlebars')
+});
+
+app.get('/other-page',function(req,res){
+  res.type('text/plain');
+  res.render('other-page');
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('postForm', context);
+});
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
